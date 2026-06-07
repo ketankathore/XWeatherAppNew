c5450d25b29802a90b6c64d36b44da1a56c4d412
Ketan Kathore <kathoreketan086@gmail.com>
2026-06-08 00:07:46 +0530

    done

--- cypress/support/e2e.js
+++
@@ -1,7 +0,0 @@
-// Support file for Cypress tests
-// Add custom commands or global configurations here
-
-// Disable unhandled exception handling for alert tests
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false;
-});
